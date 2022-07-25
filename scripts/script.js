3c9d48bce4027c67f6e9dbcbf2712d8a432f23c9
Lucas Cupertino <olucas.cdev@gmail.com>
2022-07-25 20:05:17 -0300

    finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,62 @@
 async function initQuiz() {
-    const questionTitle = document.querySelector('#title');
-    const alternatives = document.querySelectorAll('.js-input');
+    const jsonData = await fetch('../scripts/question.json').then(res => res.json());
     const label = document.querySelectorAll('#js-label');
-    const form = document.querySelector('.form');
-    const json_data = await fetch('../scripts/question.json').then(res => res.json());
-    let currAnswer = '';
-    let progress = '0/10';
+    const title = document.getElementById('title');
+    const box = document.querySelectorAll('.js-box');
+    const quizLength = jsonData.questions;
+    const startBtn = document.getElementById('submit');
+    const restart = document.getElementById('restart');
+    const welcome = document.querySelector('.welcome');
+    const quiz = document.querySelector('.quiz');
+    const finish = document.querySelector('.finish');
+    const note = document.querySelector('#note');
+    const questionBox = document.querySelector('.question-box');
+    let cont;
+    let progress;
 
-    questionTitle.innerHTML = await json_data.questions[0].title;
-    label[0].innerHTML = json_data.questions[0].alternatives[0];
-    label[1].innerHTML = json_data.questions[0].alternatives[1];
-    label[2].innerHTML = json_data.questions[0].alternatives[2];
-
-    form.addEventListener('submit', (x) => {
-        x.preventDefault();
-        questionTitle.innerHTML = '';
-
-        for (var i = 0; i < alternatives.length; i++) {
-
-            if(alternatives[i].checked) {
-                currAnswer = label[i].innerHTML;
-                console.log(currAnswer)
-            }
-        }
-
+    welcome.classList.add('activeQuiz');
+    startBtn.addEventListener('click', start);
+    restart.addEventListener('click', () => {
+        window.location.reload();
     });
 
-}
+    function start() {
+        cont = 0;
+        progress = 0
+        
+        let rightAlternative = quizLength[cont].rightAlternative;
 
+        welcome.classList.remove('activeQuiz');
+        finish.classList.remove('activeQuiz');
+        quiz.classList.add('activeQuiz');
+        questionBox.classList.add('activeQuiz');
+
+        function changeQuestion() {
+            box[cont].classList.add('active');
+            title.innerHTML = quizLength[cont].title;
+            for (i = 0; i < label.length; i++) {
+                label[i].innerHTML = quizLength[cont].alternatives[i];
+            }
+        }
+        changeQuestion();
+    
+        label.forEach((item) => {
+            item.addEventListener('click', () => {
+                if (item.innerHTML === rightAlternative) {
+                    progress ++
+                }
+                cont++
+                console.log(cont, progress)
+                if (cont < quizLength.length) {
+                    changeQuestion();
+                    rightAlternative = quizLength[cont].rightAlternative;
+                } else {
+                    quiz.classList.remove('activeQuiz');
+                    finish.classList.add('activeQuiz');
+                    note.innerHTML = `${progress}/10!`;
+                }
+            })
+        });
+    }
+}
 initQuiz();
